ba80d47d99ac895733979c20aaace379a5f22bd9
lmonty22 <lindsay.montgomery@olympian.org>
2020-04-08 17:01:38 -0400

    all done :)

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2020_04_07_184659) do
   create_table "customers", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
-    t.datetime "rupdated_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "orders", force: :cascade do |t|
