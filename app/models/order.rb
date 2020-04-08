70c691f779c9822629874293c08d6962c761f204
lmonty22 <lindsay.montgomery@olympian.org>
2020-04-08 15:16:47 -0400

    almost done

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,13 +2,9 @@ class Order < ApplicationRecord
     belongs_to :customer
     belongs_to :sales_person
     accepts_nested_attributes_for :customer
-    # validates :customer_id :price_per_ream :number_of_reams :paper_type, presence: true 
-    # curious to see if above line can work
-    # t.integer "sales_person_id"
-    # t.integer "price_per_ream"
-    # t.integer "number_of_reams"
-    # t.string "paper_type"
-    
+    validates :customer_id, presence: true
+    validates :number_of_reams, presence: true, numericality: {only_integer: true}
+    validates :price_per_ream, presence: true, numericality: {only_integer: true}
     def customer_attributes=(customer_attributes)
         customer = Customer.find_or_create_by(name: customer_attributes[:name] )
         self.customer = customer
--- app/models/sales_person.rb
+++ app/models/sales_person.rb
@@ -8,4 +8,21 @@ class SalesPerson < ApplicationRecord
     def order_count
         self.orders.count
     end
+    def sales_rev 
+        total = 0 
+        self.orders.each do |o|
+           total +=  (o.number_of_reams * o.price_per_ream)
+        end
+        total
+    end
+
+    def self.leaderboard
+        hash = Hash.new(0)
+        self.all.each do |sales_person|
+            hash["#{sales_person.name}"] = sales_person.sales_rev
+        end
+        hash = hash.sort_by {|k, v| -v}
+        return hash 
+        end
+
 end
